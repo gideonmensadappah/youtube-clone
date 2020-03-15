6aeb29ccd71cbcca821e92610d1d0ea16e6d2c16
Gideon Mensa <gideon123ewq@gmail.com>
2020-03-15 20:43:47 +0200

    Lint addVideo file

--- src/comps/main/addVideo/addVideo.jsx
+++ src/comps/main/addVideo/addVideo.jsx
@@ -1,72 +1,63 @@
-import React, { Component } from 'react';
-import {addVid} from '../../../data/UserFunctions';
-import jwt_decode from 'jwt-decode'
-
+import React, { Component } from "react";
+import { addVid } from "../../../data/UserFunctions";
+import jwt_decode from "jwt-decode";
 
 class AddVideo extends Component {
-    constructor(){
-        super()
-       
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChange = this.onChange.bind(this);
-        this.linkDestructor = this.linkDestructor.bind(this)
-       
-       
-    }
-    
-    //onSubmit Function
-    onSubmit(e){
-        e.preventDefault()
-        console.log('start process')
-        const token = localStorage.getItem('usertoken');
-        if(token == null) return console.log('error in the token') 
-        const decoded = jwt_decode(token)
-        const txt = document.getElementById('video').value;
-        const title = document.getElementById('title').value;
-        const vid =  this.linkDestructor(txt)
-
-        // video title and link are in this Obj
-        const video =  {
-            title: title,
-            video: vid,
-            uploader_id:decoded.id
-            
-        }
-        console.log(video)
-       addVid(video)
-
-
-    }
-
-    // Destruct the id from the link
-   linkDestructor(link){
-        let text = '';
-    if(link == ''){
-        console.log('please you have to bring a link')
-    
-
-    }else{
-           //seprate and get the ID of the link
-     //then return the id to console
-     // validate the arr
-    let arr =  link.split('=');
-    const _arr = arr[1]
-    let endPos =   arr[1].search('&');
-    text = _arr.slice(0,endPos);
-   
-    return text
-    }
-  
+  constructor() {
+    super();
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.linkDestructor = this.linkDestructor.bind(this);
   }
 
-    //onChange function
-    onChange(e){
-        this.setState({[e.target.name]: e.target.value })
-    }
+  //onSubmit Function
+  onSubmit(e) {
+    e.preventDefault();
+    console.log("start process");
+    const token = localStorage.getItem("usertoken");
+    if (token == null) return console.log("error in the token");
+    const decoded = jwt_decode(token);
+    const txt = document.getElementById("video").value;
+    const title = document.getElementById("title").value;
+    const vid = this.linkDestructor(txt);
 
-    state = {  }
-    render() { 
-        return (    /**  'ADD VIDEO PAGE...'
+    // video title and link are in this Obj
+    const video = {
+      title: title,
+      video: vid,
+      uploader_id: decoded.id
+    };
+    console.log(video);
+    addVid(video);
+  }
+
+  // Destruct the id from the link
+  linkDestructor(link) {
+    let text = "";
+    if (link == "") {
+      console.log("please you have to bring a link");
+    } else {
+      //seprate and get the ID of the link
+      //then return the id to console
+      // validate the arr
+      let arr = link.split("=");
+      const _arr = arr[1];
+      let endPos = arr[1].search("&");
+      text = _arr.slice(0, endPos);
+
+      return text;
+    }
+  }
+
+  //onChange function
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  state = {};
+  render() {
+    return (
+      /**  'ADD VIDEO PAGE...'
         TODO
      * 1. build the form
      * 2. build the DataBase 
@@ -77,43 +68,51 @@ class AddVideo extends Component {
      * 
      */
 
-    <React.Fragment>
-     <div className="container">
-     <div className="row">
-         
-     <div className="col-6 mt-5 my-3">
-     <div className="jumbotron jumbotron-fluid">
+      <React.Fragment>
+        <div className="container">
+          <div className="row">
+            <div className="col-6 mt-5 my-3">
+              <div className="jumbotron jumbotron-fluid">
+                <h1 className="display-4 mt-3 text-center">Add Video Page</h1>
+              </div>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="title"> Video name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="title"
+                    id="title"
+                    placeholder="title input "
+                  />
+                </div>
 
-<h1 className="display-4 mt-3 text-center">Add Video Page</h1>
-
-</div>
-       <form onSubmit={this.onSubmit}>
-       <div className="form-group">
-        <label htmlFor="title"> Video name</label>
-        <input type="text"  className="form-control" name="title" id="title"  placeholder="title input "/>    
-         </div>
- 
-        <div className="form-group">
-        <label htmlFor="video"> Add video</label>
-     <input type="text"  className="form-control" name="video" id="video"  placeholder="link input "/>
-       <button type="submit" className="btn btn-primary mt-2"> add</button>
+                <div className="form-group">
+                  <label htmlFor="video"> Add video</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="video"
+                    id="video"
+                    placeholder="link input "
+                  />
+                  <button type="submit" className="btn btn-primary mt-2">
+                    {" "}
+                    add
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6 ml-auto">
+              <video src={this.video}></video>
+            </div>
+          </div>
         </div>
-         </form>
-       </div>
-     </div>
-     <div className="row">
-         <div className="col-6 ml-auto">
-                <video 
-                src={this.video}
-                >
-
-                </video>
-         </div>
-     </div>
-     </div>
-        </React.Fragment> );
-    }
+      </React.Fragment>
+    );
+  }
 }
- 
-export default AddVideo;
 
+export default AddVideo;
