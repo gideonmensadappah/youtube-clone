e1334600cd7ea9759a547d4c98ba3bd5ae2189d4
Gideon Mensa <gideon123ewq@gmail.com>
2020-03-17 16:08:30 +0200

    Sent the id as a prop to user video profile

--- src/pages/profile/UserSideBar/UserSideBar.jsx
+++ src/pages/profile/UserSideBar/UserSideBar.jsx
@@ -1,44 +1,33 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export function UserSideBar(props) {
-     const userId = props.id;
-  
-    return(
-    
- 
-      <div className="sidebar-sticky mt-5">
+  const userid = props.id;
+
+  return (
+    <div className="sidebar-sticky mt-5">
       <ul className="nav flex-column">
-        
-      
         <li className="nav-item">
-          <Link to={'/'} className="nav-link">
-         
-          <i className="fa fa-youtube-square " aria-hidden="true"></i> All Videos
-      
+          <Link to={"/"} className="nav-link">
+            <i className="fa fa-youtube-square " aria-hidden="true"></i> All
+            Videos
           </Link>
         </li>
-   
         <li className="nav-item">
-          <Link to={`/videos/myVideos/${userId}`} className="nav-link">
-          
-          <i className="fa fa-video-camera" aria-hidden="true"></i> My Videos
-         
+          <Link
+            to={`/videos/myVideos/${userid}`}
+            userid={props.id}
+            className="nav-link"
+          >
+            <i className="fa fa-video-camera" aria-hidden="true"></i> My Videos
           </Link>
         </li>
-        <li className="nav-item" >
-          <Link to={'/add/video'} className="nav-link">
-         
-           <i className="fa fa-plus"></i> Add Videos
-      
+        <li className="nav-item">
+          <Link to={"/add/video"} className="nav-link">
+            <i className="fa fa-plus"></i> Add Videos
           </Link>
         </li>
       </ul>
-
     </div>
-
-        
-     
-    )
+  );
 }
