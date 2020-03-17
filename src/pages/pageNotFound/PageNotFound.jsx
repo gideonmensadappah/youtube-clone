ea6dfa7856cd8e6431414904cdfee3fb243a333d
Gideon Mensa <gideon123ewq@gmail.com>
2020-03-17 16:05:46 +0200

    Added page 404 (page not found)

---
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const PageNotFound = () => {
+  const style = {
+    color: "red",
+    textAlign: "center",
+    border: "10px solid black",
+    marginTop: "75px"
+  };
+  return <h1 style={style}>PageNotFound</h1>;
+};
