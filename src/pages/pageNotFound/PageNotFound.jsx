37bb082c5cc1420308d04565d4ea28008cd7920d
Gideon Mensa <gideon123ewq@gmail.com>
2020-03-18 01:17:53 +0200

    - added button to page not found - on click sends back to the home page - added style

--- src/pages/pageNotFound/PageNotFound.jsx
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
-export const PageNotFound = () => {
-  const style = {
-    color: "red",
-    textAlign: "center",
-    border: "10px solid black",
-    marginTop: "75px"
-  };
-  return <h1 style={style}>PageNotFound</h1>;
-};
+export default class PageNotFound extends Component {
+  componentDidMount() {
+    document.getElementById("btn").addEventListener("click", () => {
+      this.props.history.push("/");
+    });
+  }
+
+  render() {
+    const style = {
+      color: "red",
+      textAlign: "center",
+      border: "10px solid black",
+      marginTop: "75px"
+    };
+    return (
+      <div style={style}>
+        <h1>PageNotFound </h1>
+        <span>Oops The Page You Are Looking For Is Not Found </span>
+        <br />
+        <button id="btn" className="btn btn-primary">
+          Back to Home page
+        </button>
+      </div>
+    );
+  }
+}
