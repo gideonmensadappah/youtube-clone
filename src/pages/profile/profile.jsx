a71a0027921fe1b1cfe8362bc12a7a0f56defa40
Gideon Mensa <gideon123ewq@gmail.com>
2020-03-17 16:15:34 +0200

    update user profile and generated the user info

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,92 +1,76 @@
-import React, { Component } from 'react';
-import jwt_decode from 'jwt-decode';
-import { editProfile, userInfo } from '../../data/UserFunctions';
-import { Link } from 'react-router-dom';
-import { json } from 'body-parser';
-import { UserSideBar } from './UserSideBar/UserSideBar';
-
-
+import React, { Component } from "react";
+import jwt_decode from "jwt-decode";
+import { editProfile, userInfo } from "../../data/UserFunctions";
+import { Link } from "react-router-dom";
+import { json } from "body-parser";
+import { UserSideBar } from "./UserSideBar/UserSideBar";
 
 class Profile extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      id: '',
-      first_name: '',
-      last_name: '',
-      email: '',
-      phone: ''
-    }
-
+      id: "",
+      first_name: "",
+      last_name: "",
+      email: "",
+      phone: ""
+    };
   }
 
-
   componentDidMount() {
-  
-    const token = localStorage.getItem('usertoken')
-  
+    const token = localStorage.getItem("usertoken");
+
     // const name = localStorage.getItem('fname')
 
     if (token) {
-
-
       try {
         //{Error} = The string to be decoded is not correctly encoded.
-        const decoded = jwt_decode(token)
-        console.log(decoded)
-        this.setState({
-          id: decoded.id,
-          first_name:decoded.first_name,
-          last_name: decoded.last_name,
-          email: decoded.email,
-          phone: decoded.phone
-        })
+        const decoded = jwt_decode(token);
+
+        userInfo(decoded.id)
+          .then(res =>
+            this.setState({
+              first_name: res.data.first_name,
+              last_name: res.data.last_name,
+              email: res.data.email,
+              phone: res.data.phone,
+              id: res.data.id
+            })
+          )
+          .catch(err => console.log(err));
       } catch (err) {
-        console.log({ error: err })
+        console.log({ error: err });
       }
     } else {
-      console.log('empty token')
+      console.log("empty token");
     }
- 
-
   }
-
 
   render() {
     return (
       <React.Fragment>
-
         <div className="col-3 mr-auto">
           <UserSideBar id={this.state.id} />
-   
         </div>
 
-
-
-
         <div className="col-8 ml-auto">
-
-
           <div className="jumbotron jumbotron-fluid">
-
-            <h1 className="display-4 mt-3 text-center">Welcome {this.state.first_name}</h1>
-
+            <h1 className="display-4 mt-3 text-center">
+              Welcome {this.state.first_name}
+            </h1>
           </div>
           <table className="table">
             <thead className="thead-dark">
               <tr>
-
                 <th scope="col">First name</th>
                 <th scope="col">Last name</th>
                 <th scope="col">Email</th>
                 <th scope="col">Phone</th>
                 <th scope="col">Edit</th>
-
               </tr>
             </thead>
             <tbody>
               <tr>
-
                 <td>{this.state.first_name}</td>
                 <td>{this.state.last_name}</td>
                 <td>{this.state.email}</td>
@@ -96,19 +80,12 @@ class Profile extends Component {
                     <i className="fa fa-cog"></i>
                   </Link>
                 </td>
-
-
-
               </tr>
             </tbody>
           </table>
-
         </div>
 
-
-
         <div />
-
       </React.Fragment>
     );
   }
