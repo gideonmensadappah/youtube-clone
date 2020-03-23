8fee6c4c6fb08e2e2bbbc93cb7892eb22736f980
Gideon Mensa <gideon123ewq@gmail.com>
2020-03-23 04:08:12 +0200

    added a route for search bar

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import updateProfile from "./pages/profile/editProfile";
 import AddVideo from "./comps/main/addVideo/addVideo";
 import { UserVideo } from "./comps/main/myVideo/myVideo";
 import PageNotFound from "./pages/pageNotFound/PageNotFound";
+import SearchBar from "./comps/header/searchBar.jsx";
+import SearchPage from "./pages/search/searchPage";
 function App() {
   const style = {
     padding: "60px",
@@ -20,11 +22,12 @@ function App() {
   return (
     <Router>
       <React.Fragment>
+        <Route path="/" component={SearchBar} />
         <Header />
-        {/* <div className="container" style={style}>
+        <Switch>
+          {/* <div className="container" style={style}>
           <div className="row">
             <div className="col"> */}
-        <Switch>
           <Route path="/user/profile" component={profile} />
           <Route path="/user/update/:id" component={updateProfile} />
           <Route path="/" exact component={Main} />
@@ -33,6 +36,7 @@ function App() {
           <Route path="/user/SignUp" component={SignUp} />
           <Route path="/add/video" component={AddVideo} />
           <Route path="/videos/myVideos/:id" component={UserVideo} />
+          <Route pah="/search/:query?q=val/:" component={SearchPage} />
           <Route path="*" component={PageNotFound} />
         </Switch>
         {/* </div>
