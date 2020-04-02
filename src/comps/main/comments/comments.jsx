744b5b09f651593d7a1b25350ddfb8df37f55cba
Gideon Mensa <gideon123ewq@gmail.com>
2020-04-02 23:42:50 +0300

    Comment List Component

--- src/comps/main/comments/comments.jsx
+++ src/comps/main/comments/comments.jsx
@@ -1,26 +1,50 @@
-// import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
+import { allComments } from "../../../data/UserFunctions";
+class CommentListComponent extends Component {
+  _isMounted = false;
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeVideo: this.props.activeId,
+      commentList: []
+    };
+  }
+  componentDidMount() {
+    this._isMounted = true;
+    allComments()
+      .then(res => this.setState({ commentList: res.data }))
+      .catch(err => err);
+  }
 
-// import data from "../../../data/data.json";
-// import { allComments } from "../../../data/UserFunctions";
-// export const Comments = props => {
+  componentDidUpdate(prevProps) {
+    if (this.props !== prevProps) {
+      this.setState({
+        activeVideo: this.props.activeId,
+        commentList: this.props._arrComment
+      });
+    }
+  }
 
-//   function getComments() {
-//     console.log("in");
-//     allComments()
-//       .then(res => setCommentList(res.data))
-//       .catch(err => console.log(err));
-//   }
-
-//   useEffect(() => {
-//     getComments();
-//   }, []);
-//   const filter = commentList.filter(num => num.video_id == activeId);
-//   // console.log(filter);
-//   return (
-//     <React.Fragment>
-
-//     </React.Fragment>
-//   );
-// };
-
-// export default Comments;
+  render() {
+    const renderComments = () => {
+      return this.state.commentList
+        .filter(comments => comments.video_id === this.state.activeVideo)
+        .map(res => (
+          <div className="card" key={res.id}>
+            <div className="card-header">Quote</div>
+            <div className="card-body">
+              <blockquote className="blockquote mb-0">
+                <p>{res.comment}</p>
+                <footer className="blockquote-footer">
+                  Someone famous in{" "}
+                  <cite title="Source Title">Source Title</cite>
+                </footer>
+              </blockquote>
+            </div>
+          </div>
+        ));
+    };
+    return <>{renderComments()}</>;
+  }
+}
+export default CommentListComponent;
