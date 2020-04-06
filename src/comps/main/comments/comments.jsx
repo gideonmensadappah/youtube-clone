deab72dcb1ce3dfeb220c4f4db738dee4656e0a5
Gideon Mensa <gideon123ewq@gmail.com>
2020-04-06 19:05:49 +0300

    Took out the render comment function out of render function.

--- src/comps/main/comments/comments.jsx
+++ src/comps/main/comments/comments.jsx
@@ -6,45 +6,49 @@ class CommentListComponent extends Component {
     super(props);
     this.state = {
       activeVideo: this.props.activeId,
-      commentList: []
+      commentList: [],
     };
+
+    this.renderCommentsList = (commentList) => this.commentList();
   }
+
+  // Extract to diffrent component commentCardComponent
+  commentList = () => {
+    return this.state.commentList
+      .filter((comments) => comments.video_id === this.state.activeVideo)
+      .map((res) => (
+        <div className="card" key={res.id}>
+          <div className="card-header">Quote</div>
+          <div className="card-body">
+            <blockquote className="blockquote mb-0">
+              <p>{res.comment}</p>
+              <footer className="blockquote-footer">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </footer>
+            </blockquote>
+          </div>
+        </div>
+      ));
+  };
+
   componentDidMount() {
     this._isMounted = true;
     allComments()
-      .then(res => this.setState({ commentList: res.data }))
-      .catch(err => err);
+      .then((res) => this.setState({ commentList: res.data }))
+      .catch((err) => err);
   }
 
   componentDidUpdate(prevProps) {
     if (this.props !== prevProps) {
       this.setState({
         activeVideo: this.props.activeId,
-        commentList: this.props._arrComment
+        commentList: this.props._arrComment,
       });
     }
   }
 
   render() {
-    const renderComments = () => {
-      return this.state.commentList
-        .filter(comments => comments.video_id === this.state.activeVideo)
-        .map(res => (
-          <div className="card" key={res.id}>
-            <div className="card-header">Quote</div>
-            <div className="card-body">
-              <blockquote className="blockquote mb-0">
-                <p>{res.comment}</p>
-                <footer className="blockquote-footer">
-                  Someone famous in{" "}
-                  <cite title="Source Title">Source Title</cite>
-                </footer>
-              </blockquote>
-            </div>
-          </div>
-        ));
-    };
-    return <>{renderComments()}</>;
+    return this.renderCommentsList(this.state.commentList);
   }
 }
 export default CommentListComponent;
