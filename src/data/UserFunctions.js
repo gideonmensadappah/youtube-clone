dae69da256aa03a4c45fbb4366d6be6dc72f041c
Gideon Mensa <gideon123ewq@gmail.com>
2020-04-06 19:06:40 +0300

    Return the all comments

--- src/data/UserFunctions.js
+++ src/data/UserFunctions.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-export const signup = newUser => {
+export const signup = (newUser) => {
   return axios
     .post("/users/signup", {
       first_name: newUser.first_name,
       last_name: newUser.last_name,
       email: newUser.email,
       password: newUser.password,
-      phone: newUser.phone
+      phone: newUser.phone,
     })
-    .then(res => {
+    .then((res) => {
       console.log("Registered");
     });
 };
 
-export const signin = user => {
+export const signin = (user) => {
   return axios
     .post("/users/signin", {
       email: user.email,
-      password: user.password
+      password: user.password,
     })
-    .then(res => {
+    .then((res) => {
       localStorage.setItem("usertoken", res.data.token);
 
       console.log(res.data);
@@ -29,12 +29,12 @@ export const signin = user => {
       return res.data.token;
       // return res;
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 };
 //Edit profile
-export const editProfile = editProfile => {
+export const editProfile = (editProfile) => {
   return axios
     .put("/users/update/:id", {
       first_name: editProfile.first_name,
@@ -42,41 +42,41 @@ export const editProfile = editProfile => {
       email: editProfile.email,
       password: editProfile.password,
       phone: editProfile.phone,
-      id: editProfile.id
+      id: editProfile.id,
     })
-    .then(res => {
+    .then((res) => {
       return res.data;
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 };
 
 // User Info
-export const userInfo = id => {
+export const userInfo = (id) => {
   return axios
     .get(`/users/profile/${id}`)
-    .then(res => {
+    .then((res) => {
       return res;
     })
-    .catch(err => {
+    .catch((err) => {
       console.log({ error: err });
     });
 };
 
 //Add video
-export const addVid = addvideo => {
+export const addVid = (addvideo) => {
   return axios
     .post("/videos/add/video", {
       title: addvideo.title,
       video: addvideo.video,
-      uploader_id: addvideo.uploader_id
+      uploader_id: addvideo.uploader_id,
     })
-    .then(res => {
+    .then((res) => {
       console.log("added new video !!!");
       return res;
     })
-    .catch(error => {
+    .catch((error) => {
       console.log("there is an error!!!");
       return error;
     });
@@ -84,40 +84,40 @@ export const addVid = addvideo => {
 
 // All videos by id
 
-export const getAllVideos = userId => {
+export const getAllVideos = (userId) => {
   return axios
     .get(`/videos/myVideos/${userId}`)
-    .then(data => {
+    .then((data) => {
       const _dt = data.data;
 
       return _dt;
     })
-    .catch(error => {
+    .catch((error) => {
       return error;
     });
 };
 // Get all videos
-export const allVideos = all => {
+export const allVideos = (all) => {
   return axios
     .get("videos/all/videos")
-    .then(videos => videos)
-    .catch(err => console.log(err));
+    .then((videos) => videos)
+    .catch((err) => console.log(err));
 };
 // Add new comment
-export const addCommentFunc = comment => {
+export const addCommentFunc = (comment) => {
   return axios
     .post("/comments/addComment", comment)
-    .then(res => res)
-    .catch(err => err);
+    .then((res) => res)
+    .catch((err) => err);
 };
 //Fetch  all comments
-export const allComments = async () => {
-  return await axios.get(`/comments/getComments`);
+export const allComments = () => {
+  return axios.get(`/comments/getComments`).then((result) => result);
 };
 
-export const getDataForUserSearch = query => {
+export const getDataForUserSearch = (query) => {
   return axios
     .get(`/videos/search/query=${query}`)
-    .then(result => result)
-    .catch(error => error);
+    .then((result) => result)
+    .catch((error) => error);
 };
