b857db224cf3b1c29d07936b56d195aacc81b794
Gideon Mensa <gideon123ewq@gmail.com>
2020-04-08 18:24:16 +0300

    Extracted the video thumbnail to separate component

--- src/comps/main/main.jsx
+++ src/comps/main/main.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Comments from "./comments/comments";
 import { VideoMetaData } from "../main/VideoMetaData/VideoMetaData";
-import { NextVideo } from "../main/nextVideo/nextVideo";
 import SearchBar from "../header/searchBar";
-import { AddComments } from "./addComments/addComments";
-import { allVideos } from "../../data/UserFunctions";
+
+import VideoThumbnail from "../main/MoviesMenue/VideoThumbnail";
+
 import main from "../main/main.css";
 
 const styles = {
@@ -31,37 +31,6 @@ const Main = () => {
     source: "36LHsjX6vCo",
     title: "defult",
   });
-  const [videoList, setVideoList] = useState([]);
-
-  // Youtube Image Generator, Has to be potten in component
-  const img = (videoSource, videoTile, videoId) => {
-    return (
-      <img
-        src={`https://img.youtube.com/vi/${videoSource}/hqdefault.jpg`}
-        key={videoId}
-        style={styles.videoThumbnail}
-        alt=""
-        onClick={() => handleClickedVideo(videoSource, videoTile, videoId)}
-      />
-    );
-  };
-
-  //Handle Clicked Video Function
-  function handleClickedVideo(source, title, id) {
-    setActiveVideo({
-      id: id,
-      key: id,
-      source: source,
-      title: title,
-    });
-  }
-
-  // Use Effect
-  useEffect(() => {
-    allVideos()
-      .then((videos) => setVideoList(videos.data.videos))
-      .catch((err) => console.log(err));
-  }, []);
 
   // active video function
   function activeVid() {
@@ -97,13 +66,12 @@ const Main = () => {
       <div className="container">
         <div className="boxs" id="comment">
           {" "}
-          Add comment below
-          <AddComments id={activeVideo.id} />
+          <Comments activeVideoId={activeVideo.id} />
         </div>
 
         {/* <NextVideo /> */}
         <div className="boxs">
-          {videoList.map((video) => img(video.source, video.title, video.id))}
+          <VideoThumbnail setActiveVideo={setActiveVideo} />
         </div>
       </div>
     </React.Fragment>
